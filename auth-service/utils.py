8ca8c0116a7c2cde969d2424656a58df0ce124d0
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-24 02:36:03 +0500

    hash_password return type changed to str from bytes

--- auth-service/utils.py
+++ auth-service/utils.py
@@ -47,18 +47,21 @@ def decode_jwt(
 # Максимальная длина для корректной работы это 72 символа
 def hash_password(
         password: str
-) -> bytes:
+) -> str:
     salt: bytes = bcrypt.gensalt()
     pwd_bytes: bytes = password.encode()
     hashed_pwd: bytes = bcrypt.hashpw(password=pwd_bytes,
                                       salt=salt)
-    return hashed_pwd.decode("utf-8")
+    hashed_pwd: str = hashed_pwd.decode()
+    return hashed_pwd
 
 
 def check_password(
         password: str,
-        hashed_password: bytes,
+        hashed_password: str,
 ) -> bool:
+    hashed_password: bytes = hashed_password.encode()
     result: bool = bcrypt.checkpw(password=password.encode(),
                                   hashed_password=hashed_password)
     return result
+
