18cdbfb06e34550456216abd487ea8131d99916d
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-24 16:36:16 +0500

    Add UnauthorizedHTTPError to exc.py

--- auth-service/exc.py
+++ auth-service/exc.py
@@ -23,3 +23,10 @@ class InterServerHTTPError(HTTPException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                          detail="Something went wrong on server")
+
+
+class UnauthorizedHTTPError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password")
