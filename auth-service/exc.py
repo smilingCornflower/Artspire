e7fbde7a1582f9f22f3931c7a10a6cb670c1ff10
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-24 17:48:28 +0500

    Fixed SyntxError: SQLError does not accept keyword arguments

--- auth-service/exc.py
+++ auth-service/exc.py
@@ -35,4 +35,4 @@ class UnauthorizedHTTPError(HTTPException):
 
 class UserNotFoundSQLError(SQLAlchemyError):
     def __init__(self, message: str = "User not found in the database"):
-        super().__init__(message=message)
+        super().__init__(message)
