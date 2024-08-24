4feb2048c2019ab3adeb33d309692a425f49049e
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-24 15:59:34 +0500

    Add new exception: WeakPasswordHTTPError

--- auth-service/exc.py
+++ auth-service/exc.py
@@ -13,3 +13,7 @@ class EmailAlreadyExistsHTTPError(HTTPException):
                          detail="User with this email already exists")
 
 
+class WeakPasswordHTTPError(HTTPException):
+    def __init__(self, detail: str = "Password is too weak"):
+        super().__init__(status_code=400,
+                         detail=detail)
