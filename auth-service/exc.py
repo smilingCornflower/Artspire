9c209aad67215abaf7ab0d04ddbe507c7ff0627c
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-24 16:31:54 +0500

    Add InternalServerHTTPError to exc.py

--- auth-service/exc.py
+++ auth-service/exc.py
@@ -1,19 +1,25 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 
 
 class UsernameAlreadyExistHTTPError(HTTPException):
     def __init__(self):
-        super().__init__(status_code=400,
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
                          detail="User with this username already exists")
 
 
 class EmailAlreadyExistsHTTPError(HTTPException):
     def __init__(self):
-        super().__init__(status_code=400,
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
                          detail="User with this email already exists")
 
 
 class WeakPasswordHTTPError(HTTPException):
     def __init__(self, detail: str = "Password is too weak"):
-        super().__init__(status_code=400,
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
                          detail=detail)
+
+
+class InterServerHTTPError(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                         detail="Something went wrong on server")
