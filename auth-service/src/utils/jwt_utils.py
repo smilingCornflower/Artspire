5a703330f9160b4a946bac90c44f225f45bcfb4c
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-26 00:48:34 +0500

    Added three methods: create_jwt, create_access_jwt, create_refresh_jwt

--- auth-service/src/utils/jwt_utils.py
+++ auth-service/src/utils/jwt_utils.py
@@ -1,9 +1,15 @@
 import jwt
+
+import jwt_utils
 from config import settings
 from datetime import datetime, timedelta, timezone
 from copy import deepcopy
 
 from typing import Any
+
+TOKEN_TYPE_FIELD = "type"
+ACCESS_TOKEN_TYPE = "access"
+REFRESH_TOKEN_TYPE = "refresh"
 
 
 def encode_jwt(
@@ -39,3 +45,35 @@ def decode_jwt(
         algorithms=[algorithm],
     )
     return decoded
+
+
+def create_jwt(
+        token_type: str,
+        token_data: dict,
+        expire_minutes: int = settings.jwt_access_token_expire_minutes,
+) -> str:
+    jwt_payload: dict = {TOKEN_TYPE_FIELD: token_type}
+    jwt_payload.update(token_data)
+    encoded: str = encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes
+    )
+    return encoded
+
+
+def create_access_jwt(token_data: dict) -> str:
+    access_token: str = create_jwt(
+        token_type=ACCESS_TOKEN_TYPE,
+        token_data=token_data,
+        expire_minutes=settings.jwt_access_token_expire_minutes,
+    )
+    return access_token
+
+
+def create_refresh_jwt(token_data: dict) -> str:
+    refresh_token: str = create_jwt(
+        token_type=REFRESH_TOKEN_TYPE,
+        token_data=token_data,
+        expire_minutes=settings.jwt_refresh_token_expire_minutes,
+    )
+    return refresh_token
