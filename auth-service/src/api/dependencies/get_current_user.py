20078a500d8a1e1ce069e3d0193e957b70aa6a87
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-26 04:01:12 +0500

    Added dependency get_current_user.py

---
+++ auth-service/src/api/dependencies/get_current_user.py
@@ -0,0 +1,19 @@
+from fastapi import Depends
+from utils.jwt_utils import ACCESS_TOKEN_TYPE
+from schemas.users import UserReadSchema
+from config import settings, logger
+from .get_decoded_token import get_decoded_access_token
+
+
+def get_current_user(
+        user_data: dict = Depends(get_decoded_access_token)
+) -> UserReadSchema:
+    print(1)
+    logger.debug(f"user_data: {user_data}")
+    user: UserReadSchema = UserReadSchema(
+        id=user_data["sub"],
+        username=user_data["username"],
+        email=user_data["email"],
+        profile_image=user_data["profile_image"],
+    )
+    return user
