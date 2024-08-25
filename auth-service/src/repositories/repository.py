db1b49dd5d4258cbb00150f48d71c09be1b8f482
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-25 21:46:15 +0500

    Added annotation and logger

--- auth-service/src/repositories/repository.py
+++ auth-service/src/repositories/repository.py
@@ -1,8 +1,22 @@
 from abc import ABC, abstractmethod
 from sqlalchemy import insert, select, and_
 from database.db import db_manager
-
+from config import settings
 from schemas.users import UserEntity
+
+# Annotation
+from sqlalchemy import Select, Insert, ChunkedIteratorResult
+from sqlalchemy.sql.expression import BinaryExpression
+from sqlalchemy.orm import DeclarativeMeta
+
+# Logging
+from loguru import logger
+
+logger.add(settings.logs_path,
+           format="{time:YYYY-MM-DD HH:mm:ss} | {level} | [{file} | {function} | {line}] \n \t {message}",
+           level="DEBUG",
+           rotation="10 MB",
+           compression="zip")
 
 
 class AbstractRepository(ABC):
@@ -21,19 +35,21 @@ class SQLAlchemyRepository(AbstractRepository):
     async def add_one(self, data: dict) -> int:
         async with db_manager.async_session_maker() as session:
             async with session.begin():
-                stmt = insert(self.model).values(**data).returning(self.model.id)
-                result = await session.execute(stmt)
-                result = result.scalar_one()
+                stmt: Insert = insert(self.model).values(**data).returning(self.model.id)
+                result: ChunkedIteratorResult = await session.execute(stmt)
+                result: DeclarativeMeta = result.scalar_one()
                 return result
 
-    async def find_all(self, filter_by: dict = None) -> list[UserEntity]:
+    async def find_all(self, filter_by: dict = None) -> list:
         async with db_manager.async_session_maker() as session:
-            stmt = select(self.model)
+            stmt: Select = select(self.model)
             if filter_by:
-                conditions = [getattr(self.model, key) == value for key, value in filter_by.items()]
+                conditions: list[BinaryExpression] = [
+                    getattr(self.model, key) == value for key, value in filter_by.items()
+                ]
                 if conditions:
-                    stmt = stmt.where(and_(*conditions))
+                    stmt: Select = stmt.where(and_(*conditions))
 
-            result = await session.execute(stmt)
-            result = [row[0].to_entity() for row in result.all()]
+            result: ChunkedIteratorResult = await session.execute(stmt)
+            result: list = [row[0].to_entity() for row in result.all()]
             return result
