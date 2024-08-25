9932767ba2387d3129d63c8e29f600eac8e90d87
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-25 21:51:34 +0500

    Added function validate_user() to UserService

--- auth-service/src/services/users.py
+++ auth-service/src/services/users.py
@@ -1,13 +1,28 @@
 from repositories.repository import AbstractRepository
-from schemas.users import UserCreateSchema
-from utils.password import hash_password
-
+from config import settings
+from schemas.users import (
+    UserCreateSchema,
+    UserLoginSchema,
+    UserEntity,
+)
+from schemas.tokens import TokenInfoSchema
+from utils.password import hash_password, check_password
+from utils.jwt import encode_jwt
 
 from exceptions.http import (
     UsernameAlreadyExistHTTPException,
     EmailAlreadyExistsHTTPException,
     WeakPasswordHTTPException,
+    UnauthorizedHTTPException,
 )
+
+from loguru import logger
+
+logger.add(settings.logs_path,
+           format="{time:YYYY-MM-DD HH:mm:ss} | {level} | [{file} | {function} | {line}] \n \t {message}",
+           level="DEBUG",
+           rotation="10 MB",
+           compression="zip")
 
 
 class UserService:
@@ -36,3 +51,29 @@ class UserService:
         new_user_id: int = await self.user_repo.add_one(data=new_user_data)
 
         return new_user_id
+
+    async def validate_user(self, user: UserLoginSchema) -> TokenInfoSchema:
+        user_by_username: list[UserEntity] = await self.user_repo.find_all({"username": user.username})
+
+        if not user_by_username:
+            raise UnauthorizedHTTPException
+
+        user_entity: UserEntity = user_by_username[0]
+        correct_password: str = user_entity.hashed_password
+        check_password_result: bool = check_password(user.password, correct_password)
+
+        if not check_password_result:
+            raise UnauthorizedHTTPException
+
+        to_jwt_payload: dict = {
+            "sub": user_entity.id,
+            "username": user_entity.username,
+            "email": user_entity.email,
+            "profile_image": user_entity.profile_image,
+        }
+        encoded: str = encode_jwt(payload=to_jwt_payload)
+        token_data: TokenInfoSchema = TokenInfoSchema(
+            access_token=encoded,
+            token_type="Bearer"
+        )
+        return token_data
