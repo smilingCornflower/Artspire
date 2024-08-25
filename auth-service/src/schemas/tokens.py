4fd3b83812c7858c5ac8594b8639f7a17302168c
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-26 03:57:45 +0500

    Refresh token in TokenInfoSchem is optional now

--- auth-service/src/schemas/tokens.py
+++ auth-service/src/schemas/tokens.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel
 
 class TokenInfoSchema(BaseModel):
     access_token: str
-    refresh_token: str
+    refresh_token: str | None = None
     token_type: str = "Bearer"
 
