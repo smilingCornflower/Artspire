c2cf7cbb085bc305cdc2f13062dfe3532024f525
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-25 21:48:47 +0500

    Added annotation

--- auth-service/src/api/dependencies.py
+++ auth-service/src/api/dependencies.py
@@ -2,5 +2,5 @@ from services.users import UserService
 from repositories.users import UserRepository
 
 
-def get_user_service():
+def get_user_service() -> UserService:
     return UserService(user_repo=UserRepository())
