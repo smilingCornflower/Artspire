1cb3ab956e8f81f59d79d3946e8de79e6c6dee08
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-25 17:40:06 +0500

    RoleOrm moved to models/roles.py

---
+++ auth-service/src/models/roles.py
@@ -0,0 +1,15 @@
+from database.base import Base
+from sqlalchemy.orm import Mapped as M
+from sqlalchemy.orm import mapped_column as mc
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+from users import UserOrm
+
+
+class RoleOrm(Base):
+    __tablename__ = "roles"
+
+    id: M[int] = mc(primary_key=True)
+    name: M[str] = mc(String(length=50), unique=True, nullable=False)
+
+    users = relationship("UserOrm", back_populates="role")
