42a1c39a7a8f68871859cc979b06c23f263311c0
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-26 04:02:01 +0500

    Added new route /users/me

--- auth-service/src/api/router.py
+++ auth-service/src/api/router.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, Form, Depends
+from fastapi import APIRouter, Depends
 from schemas.users import (
     UserCreateSchema,
     UserLoginSchema,
+    UserReadSchema,
 )
+from config import logger
+from services.users import UserService
 from schemas.tokens import TokenInfoSchema
-from .dependencies import get_user_service
+from .dependencies import get_user_service, get_current_user, get_decoded_refresh_token
 
 router = APIRouter(
     prefix="/users",
@@ -15,16 +18,24 @@ router = APIRouter(
 @router.post("/register")
 async def register_user(
         user_create_data: UserCreateSchema,
-        user_service=Depends(get_user_service)
+        user_service: UserService = Depends(get_user_service)
 ) -> int:
     new_user_id: int = await user_service.add_user(user_create_data)
     return new_user_id
 
 
-@router.post("/login")
+@router.post("/login", response_model=TokenInfoSchema)
 async def login_user(
         user_login_data: UserLoginSchema,
         user_service=Depends(get_user_service)
 ) -> TokenInfoSchema:
     token_data: TokenInfoSchema = await user_service.validate_user(user_login_data)
     return token_data
+
+
+
+@router.get("/me")
+async def current_user_info(
+        user: UserReadSchema = Depends(get_current_user)
+):
+    return user
