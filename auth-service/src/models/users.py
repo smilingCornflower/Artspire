6c28c0c1190c39dd593db0b717b69abe84d02268
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-25 18:19:12 +0500

    Added to_entity() method and had been set default role_id = 1

--- auth-service/src/models/users.py
+++ auth-service/src/models/users.py
@@ -9,6 +9,8 @@ from sqlalchemy.orm import relationship
 
 from roles import RoleOrm
 
+from schemas.users import UserEntity
+
 
 class UserOrm(Base):
     __tablename__ = "users"
@@ -18,7 +20,7 @@ class UserOrm(Base):
     username: M[str] = mc(String(length=50), unique=True, nullable=False)
     email: M[str] = mc(String(length=255), unique=True, nullable=False)
     hashed_password: M[str] = mc(String(length=64), unique=False, nullable=False)
-    role_id: M[int] = mc(ForeignKey("roles.id"), nullable=False)
+    role_id: M[int] = mc(ForeignKey("roles.id"), default=1, server_default=1, nullable=False)
 
     is_active: M[bool] = mc(default=True, nullable=False)
     is_verified: M[bool] = mc(default=False, nullable=False)
@@ -30,3 +32,14 @@ class UserOrm(Base):
     )
 
     role = relationship("RoleOrm", back_populates="users")
+
+    def to_entity(self) -> UserEntity:
+        user_entity = UserEntity(
+            id=self.id,
+            username=self.username,
+            email=self.email,
+            profile_image=self.profile_image,
+            role_id=self.is_active,
+            is_active=self.is_active,
+        )
+        return user_entity
