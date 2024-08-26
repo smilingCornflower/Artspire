dfbc14a9f7d60a16a95e92abeb5aa9d1a2534389
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-27 02:49:22 +0500

    Create art-service

---
+++ art-service/src/__init__.py
@@ -0,0 +1,11 @@
+from . import api
+from . import database
+from . import exceptions
+from . import models
+from . import rabbit
+from . import repositories
+from . import schemas
+from . import services
+from . import utils
+from . import config
+
