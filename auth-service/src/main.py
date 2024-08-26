cf4adc432eb33abd2adb9d191d0fd9f770d1e0da
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-26 05:11:16 +0500

    Fixed Cors Headers problem

--- auth-service/src/main.py
+++ auth-service/src/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from api.router import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="Artspire-Auth"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router)
