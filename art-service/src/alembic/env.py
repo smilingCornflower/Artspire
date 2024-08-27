6b83060553087215927fe7f30bb5f3d44903a912
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-27 14:59:51 +0500

    Initialized alembic

--- art-service/src/alembic/env.py
+++ art-service/src/alembic/env.py
@@ -6,10 +6,9 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from config import settings, art_dir
 from database.base import Base
-from config import settings
 import models
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -24,7 +23,8 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-config.set_main_option("sqlalchemy.url", settings.db_url)
+config.set_main_option("sqlalchemy.url", settings.db.get_db_url())
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -57,7 +57,9 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(connection=connection,
+                      target_metadata=target_metadata,
+                      )
 
     with context.begin_transaction():
         context.run_migrations()
