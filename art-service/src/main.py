cd9337f8cd319ceee1f3a145d5763b4e9ac9a3ed
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-31 00:31:19 +0500

    Added lifespan function for main.py

--- art-service/src/main.py
+++ art-service/src/main.py
@@ -1,9 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
 from api.router import router as arts_router
+from rabbit.ping_sender import ping_sender
+import asyncio
+from asyncio import Task
+
+
+async def async_lifespan(app: FastAPI):
+    ping_sender_task: "Task" = asyncio.create_task(ping_sender())
+    yield
+    ping_sender_task.cancel()
 
 app = FastAPI(
-    title="Artspire-Arts"
+    title="Artspire-Arts",
+    lifespan=async_lifespan,
 )
 
 app.include_router(arts_router)
