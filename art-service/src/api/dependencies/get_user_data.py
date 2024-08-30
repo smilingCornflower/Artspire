c0964a9834489e63263ecade7389ae9814773f9c
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-30 06:58:03 +0500

    Changed get_user_data.py. It returns UserEntity or raise UserNotAuthorizedException

--- art-service/src/api/dependencies/get_user_data.py
+++ art-service/src/api/dependencies/get_user_data.py
@@ -3,6 +3,8 @@ from fastapi import Depends
 from fastapi.security.http import HTTPBearer
 from rabbit.jwt_client import run_jwt_client
 from config import logger
+from exceptions.http_exc import UnauthorizedHTTPException
+from schemas.entities import UserEntity
 
 if TYPE_CHECKING:
     from fastapi.security.http import HTTPAuthorizationCredentials
@@ -11,9 +13,17 @@ http_bearer = HTTPBearer()
 
 async def get_user_data(
         credentials: "HTTPAuthorizationCredentials" = Depends(http_bearer)
-) -> dict:
+) -> UserEntity:
     token: str = credentials.credentials
     logger.debug(f"Received token: {token}")
     decoded: dict = await run_jwt_client(body=token)
-    logger.info(f"Token is decoded: {decoded}")
-    return decoded
+    if not decoded["is_valid"]:
+        raise UnauthorizedHTTPException
+    user_data: "UserEntity" = UserEntity(
+        id=decoded["decoded"]["sub"],
+        username=decoded["decoded"]["username"],
+        email=decoded["decoded"]["email"],
+        profile_image=decoded["decoded"]["profile_image"],
+    )
+    logger.info(f"user_data: {user_data}")
+    return user_data
