d48fae0c28de8004eccb91cfd018779e80c3f1e3
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-30 06:54:25 +0500

    image converted fixed: Old version saved file in local storage of pc

--- art-service/src/utils/img.py
+++ art-service/src/utils/img.py
@@ -7,9 +7,12 @@ from PIL import Image
 
 async def convert_to_jpg(upload_file: BinaryIO) -> BinaryIO:
     logger.info("Started converting the image")
-    img = Image.open(upload_file)
-    img = img.convert('RGB')
-    img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='JPEG')
-    img_byte_arr.seek(0)
-    return img_byte_arr
+    try:
+        image = Image.open(upload_file)
+        output = io.BytesIO()
+        image.save(output, format="JPEG")
+        output.seek(0)
+        return output
+    except OSError as err:
+        logger.error("OSError occurred during image processing")
+        raise err
