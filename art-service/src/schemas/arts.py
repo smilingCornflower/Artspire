fe406fc1f4f9892af9b602080f52e753bd1c79cf
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-30 06:55:00 +0500

    Added to_dict() method for ArtCreateSchema

--- art-service/src/schemas/arts.py
+++ art-service/src/schemas/arts.py
@@ -13,4 +13,16 @@ class ArtCreateSchema(BaseModel):
     title: str | None
     tags: list[str]
     url: str
+    blob_name: str
     url_generated_at: datetime
+
+    def to_dict(self) -> dict:
+        result: dict = {
+            "user_id": self.user_id,
+            "title": self.title,
+            "tags": self.tags,
+            "url": self.url,
+            "blob_name": self.blob_name,
+            "url_generated_at": self.url_generated_at,
+        }
+        return result
