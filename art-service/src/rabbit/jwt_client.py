fd7344a2b86353a54444676784c3e20aab728bb7
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-30 06:53:38 +0500

    Added annotation for jwt_client.py

--- art-service/src/rabbit/jwt_client.py
+++ art-service/src/rabbit/jwt_client.py
@@ -6,7 +6,7 @@ from exceptions.http_exc import InternalServerErrorHTTPException
 JWT_REQUEST: str = "jwt_request"
 
 
-async def run_jwt_client(body: str):
+async def run_jwt_client(body: str) -> dict:
     json_result: str = await run_rpc_client(body=body, routing_key=JWT_REQUEST)
     try:
         result: dict = json.loads(json_result)
