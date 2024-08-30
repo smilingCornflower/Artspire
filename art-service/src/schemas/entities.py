9e90045191848e906c94111b0cce1a8b789f3885
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-30 06:55:15 +0500

    Added UserEntity and Tagentity

--- art-service/src/schemas/entities.py
+++ art-service/src/schemas/entities.py
@@ -1,5 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class BaseEntity(BaseModel):
     id: int
+
+
+class UserEntity(BaseEntity):
+    id: int
+    username: str
+    email: EmailStr
+    profile_image: str | None
+
+
+class TagEntity(BaseEntity):
+    id: int
+    name: str
