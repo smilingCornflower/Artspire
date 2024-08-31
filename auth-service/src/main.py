4333cba354d90b7b1bdb316d08f216a7d7d8f11e
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-31 18:41:13 +0500

    Jwt server setup and reload in jwt_server() function

--- auth-service/src/main.py
+++ auth-service/src/main.py
@@ -3,7 +3,7 @@ import asyncio
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from rabbit.jwt_server import run_jwt_server
+from rabbit.jwt_server import jwt_server
 from api.router import router as auth_router
 from contextlib import asynccontextmanager
 from config import logger
@@ -12,15 +12,8 @@ from config import logger
 @asynccontextmanager
 async def async_lifespan(app_name: FastAPI):
     logger.info(f"{app_name} Started")
-    jwt_server_task = asyncio.create_task(run_jwt_server())
-
+    await jwt_server()
     yield
-    jwt_server_task.cancel()
-    try:
-        await jwt_server_task
-        logger.critical("JWT server not cancelled")
-    except asyncio.CancelledError:
-        logger.info("JWT server task was cancelled.")
 
 
 app = FastAPI(
@@ -29,7 +22,7 @@ app = FastAPI(
 )
 
 app.add_middleware(
-    CORSMiddleware,     # noqa
+    CORSMiddleware,  # noqa
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
--- auth-service/src/rabbit/jwt_server.py
+++ auth-service/src/rabbit/jwt_server.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from .rpc_server import RmqRpcServer
 from utils.jwt_utils import decode_jwt, ACCESS_TOKEN_TYPE
 from jwt.exceptions import PyJWTError
@@ -36,17 +38,38 @@ class JwtRpcServer(RmqRpcServer):
         return response_str
 
 
-async def run_jwt_server() -> None:
-    jwt_server = JwtRpcServer()
+async def _run_jwt_rpc_server() -> None:
+    jwt_rpc_server: "JwtRpcServer" = JwtRpcServer()
     try:
-        await jwt_server.connect()
-        await jwt_server.process_messages()
-    except AMQPException as err:
-        logger.critical(f"AMQP error: {err}")
-        await jwt_server.cleanup()
-    except PyJWTError as err:
-        logger.error(f"JWT error: {err}")
-        await jwt_server.cleanup()
-    except Exception as err:
-        logger.error(f"Unexpected error: {err}")
-        await jwt_server.cleanup()
+        await jwt_rpc_server.connect()
+        await jwt_rpc_server.process_messages()
+    except (AMQPException, PyJWTError, Exception) as err:
+        logger.error(f"error: {err}")
+        await jwt_rpc_server.cleanup()
+
+
+async def jwt_server():
+    jwt_server_task: "asyncio.Task | None" = None
+    try:
+        while True:
+            if jwt_server_task is None or jwt_server_task.done():
+                jwt_server_task = asyncio.create_task(_run_jwt_rpc_server())
+
+            try:
+                await jwt_server_task
+            except asyncio.CancelledError:
+                logger.info("JWT server task was cancelled.")
+                break
+            except Exception as e:
+                logger.error(f"JWT server task encountered an error: {e}", exc_info=True)
+                logger.info("Restarting JWT server task...")
+
+            await asyncio.sleep(1)
+    finally:
+        if jwt_server_task:
+            jwt_server_task.cancel()
+            try:
+                await jwt_server_task
+                logger.critical("JWT server not cancelled")
+            except asyncio.CancelledError:
+                logger.info("JWT server task was cancelled.")
