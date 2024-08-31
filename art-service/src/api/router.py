bcd6039a4cbc229d7b80265e3f642501f0384370
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-08-31 23:38:16 +0500

    add endpoint: POST /tags

--- art-service/src/api/router.py
+++ art-service/src/api/router.py
@@ -1,16 +1,28 @@
 from typing import TYPE_CHECKING
+
 from fastapi import APIRouter, Depends, UploadFile
 from fastapi.security.http import HTTPBearer
-from .dependencies.get_user_data import get_user_data
-from .dependencies.get_art_service import get_art_service
-from exceptions.http_exc import UnauthorizedHTTPException, FailedUploadHttpException
+
+from services.tags import TagsService
 from schemas.arts import ArtUploadSchema
 from schemas.entities import UserEntity
+
+from .dependencies.get_user_data import get_user_data
+from .dependencies.get_arts_service import get_arts_service
+from .dependencies.get_tags_service import get_tags_service
+
+from exceptions.http_exc import (
+    UnauthorizedHTTPException,
+    FailedUploadHttpException,
+    ForbiddenHTTPException,
+)
+
+from rabbit.jwt_client import run_jwt_client
+
 if TYPE_CHECKING:
     from fastapi.security.http import HTTPAuthorizationCredentials
     from services.arts import ArtsService
 
-from rabbit.jwt_client import run_jwt_client
 
 router = APIRouter(
     prefix="/arts",
@@ -23,7 +35,7 @@ async def post_art(
         art_file: UploadFile,
         art_tags: str,
         art_title: str | None,
-        art_service: "ArtsService" = Depends(get_art_service),
+        art_service: "ArtsService" = Depends(get_arts_service),
         user_data: "UserEntity" = Depends(get_user_data),
 ) -> int:
     art_upload_data: "ArtUploadSchema" = ArtUploadSchema(
@@ -36,3 +48,15 @@ async def post_art(
     except FailedUploadHttpException as err:
         raise
     return new_art_id
+
+
+@router.post("/tags")
+async def post_tag(
+        tag_name: str,
+        user_data: "UserEntity" = Depends(get_user_data),
+        tag_service: "TagsService" = Depends(get_tags_service)
+) -> int:
+    if user_data.role_id != 2:
+        raise ForbiddenHTTPException
+    new_tag_id: int = await tag_service.add_tag(tag_name)
+    return new_tag_id
