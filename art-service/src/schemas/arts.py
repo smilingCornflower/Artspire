7d0144bcbedd278d9b51429bd602113f93367e5d
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-03 15:20:48 +0500

    Deleted method to_dict(). BaseModel already have this method, i didn't know it :D

--- art-service/src/schemas/arts.py
+++ art-service/src/schemas/arts.py
@@ -15,14 +15,3 @@ class ArtCreateSchema(BaseModel):
     url: str
     blob_name: str
     url_generated_at: datetime
-
-    def to_dict(self) -> dict:
-        result: dict = {
-            "user_id": self.user_id,
-            "title": self.title,
-            "tags": self.tags,
-            "url": self.url,
-            "blob_name": self.blob_name,
-            "url_generated_at": self.url_generated_at,
-        }
-        return result
