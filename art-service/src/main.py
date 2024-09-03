8cb164e34356c5a29bb3cbd989d85a42108d0224
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-03 15:27:18 +0500

    Added CORS and redirect to main.py

--- art-service/src/main.py
+++ art-service/src/main.py
@@ -1,22 +1,34 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from api.router import router as arts_router
-from rabbit.ping_sender import ping_sender
+from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 from asyncio import Task
 
 
 async def async_lifespan(app: FastAPI):
-    ping_sender_task: "Task" = asyncio.create_task(ping_sender())
     yield
-    ping_sender_task.cancel()
 
 app = FastAPI(
     title="Artspire-Arts",
-    lifespan=async_lifespan,    # noqa
+    lifespan=async_lifespan,  # noqa
 )
 
+app.add_middleware(
+    CORSMiddleware, # noqa
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(arts_router)
+
+
+@app.get("/")
+async def redirect_to_docs() -> RedirectResponse:
+    return RedirectResponse(url="/docs")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
