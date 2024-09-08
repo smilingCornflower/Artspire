63ed56cb9d52758460da45ec7ba7ab1ab1c03549
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-08 17:00:44 +0500

    Create UserSavedArtsOrm

---
+++ art-service/src/models/user_saved_arts.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped as M
+from sqlalchemy.orm import mapped_column as mc
+from sqlalchemy import ARRAY, Integer
+from database.base import Base
+from schemas.entities import UserSavedArtsEntity
+
+
+class UserSavedArtsOrm(Base):
+    __tablename__ = "user_saved_arts"
+
+    id: M[int] = mc(primary_key=True, index=True)
+    arts: M[list[int]] = mc(ARRAY(Integer), nullable=True)
