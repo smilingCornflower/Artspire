e0b85981efd57cd3535cb95d3d30512855503032
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-09 16:12:44 +0500

    Create UserToServiceRepository

---
+++ art-service/src/repositories/users_to_saves.py
@@ -0,0 +1,36 @@
+from typing import TYPE_CHECKING
+from .repository import SQLAlchemyRepository
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from models.users_to_saves import UsersToSavesOrm
+from database.db import db_manager
+
+from config import logger
+
+if TYPE_CHECKING:
+    from sqlalchemy.dialects.postgresql import Insert
+    from sqlalchemy import ChunkedIteratorResult
+
+
+class UsersToSavesRepository(SQLAlchemyRepository):
+    model = UsersToSavesOrm
+
+    async def add_one(self, data: dict) -> int:
+        logger.debug(f"Insert data: {data}")
+
+        async with db_manager.async_session_maker() as session:
+            async with session.begin():
+                try:
+                    stmt: "Insert" = insert(self.model).values(**data)
+                    stmt: "Insert" = stmt.on_conflict_do_nothing(
+                        index_elements=["user_id", "art_id"]
+                    )
+                    result: "ChunkedIteratorResult" = await session.execute(stmt)
+                    return result.rowcount
+
+                except IntegrityError as err:
+                    logger.error(f"IntegrityError: {err}")
+                    raise err
+                except SQLAlchemyError as err:
+                    logger.error(f"SQLAlchemyError: {err}")
+                    raise err
