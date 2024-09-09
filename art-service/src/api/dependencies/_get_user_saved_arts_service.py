5c3770166b951814da3a46cd91b08afdd527994b
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-09 17:38:21 +0500

    Deleted not needed files

--- art-service/src/api/dependencies/_get_user_saved_arts_service.py
+++
@@ -1,6 +0,0 @@
-from repositories.user_saved_arts import UserSavedArtsRepository
-from services.user_saved_arts import UserSavedArtsService
-
-
-def get_user_saved_arts_service() -> "UserSavedArtsService":
-    return UserSavedArtsService(UserSavedArtsRepository())
--- art-service/src/repositories/user_saved_arts.py
+++
@@ -1,7 +0,0 @@
-from .repository import SQLAlchemyRepository
-from models.user_saved_arts import UserSavedArtsOrm
-
-
-class UserSavedArtsRepository(SQLAlchemyRepository):
-    model = UserSavedArtsOrm
-
