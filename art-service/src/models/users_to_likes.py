23d720b27090f4d9b6efb430789ad1a1e2454faa
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-10 15:00:53 +0500

    Create model users_to_likes

---
+++ art-service/src/models/users_to_likes.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped as M
+from sqlalchemy.orm import mapped_column as mc
+from sqlalchemy import ForeignKey
+from database.base import Base
+
+
+class UsersToLikes(Base):
+    __tablename__ = "users_to_likes"
+
+    user_id: M[int] = mc(primary_key=True)
+    art_id: M[int] = mc(ForeignKey("arts.id"), primary_key=True)
+
