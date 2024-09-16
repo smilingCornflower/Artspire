b150ac2477c08cd3d659d20db2ab2dab9930a91c
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-16 23:16:12 +0500

    Added method get_tags_by_name_part() for TagsRepository

--- art-service/src/repositories/tags.py
+++ art-service/src/repositories/tags.py
@@ -1,6 +1,35 @@
+from database.db import db_manager
 from .repository import SQLAlchemyRepository
 from models.tags import TagOrm
+from typing import TYPE_CHECKING
+from config import logger
+from sqlalchemy import select, func
+from sqlalchemy.exc import SQLAlchemyError
+from schemas.entities import BaseEntity
+if TYPE_CHECKING:
+    from sqlalchemy import Select, Result
 
 
 class TagRepository(SQLAlchemyRepository):
     model = TagOrm
+
+    async def get_tags_by_name_part(self, tag_part: str):
+        logger.debug(f"Selecting from {self.model.__name__} with tag_part: {tag_part}")
+        async with db_manager.async_session_maker() as session:
+            tag_part: str = tag_part + "%"
+            stmt: "Select" = (
+                select(self.model)
+                .filter(func.lower(self.model.name)
+                        .like(tag_part)
+                        )
+            )
+
+            try:
+                result: "Result" = await session.execute(stmt)
+            except SQLAlchemyError as err:
+                logger.error(f"SQLAlchemyError: {err}")
+                raise err
+            rows = result.all()
+            entities: "list[BaseEntity]" = [row[0].to_entity() for row in rows]
+            logger.info(f"Found {len(entities)} entities")
+            return entities
