d99b0a3ea0c0e9cb2db480a6c3178044760760ba
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-24 15:26:02 +0500

    to_entity() in UsersToSavesOrm

--- art-service/src/models/users_to_saves.py
+++ art-service/src/models/users_to_saves.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Mapped as M
 from sqlalchemy.orm import mapped_column as mc
 from sqlalchemy import ForeignKey
 from database.base import Base
+from schemas.entities import UsersToSavesEntity
 
 
 class UsersToSavesOrm(Base):
@@ -10,3 +11,9 @@ class UsersToSavesOrm(Base):
     user_id: M[int] = mc(primary_key=True)
     art_id: M[int] = mc(ForeignKey("arts.id"), primary_key=True)
 
+    def to_entity(self) -> "UsersToSavesEntity":
+        entity = UsersToSavesEntity(
+            user_id=self.user_id,
+            art_id=self.art_id,
+        )
+        return entity
