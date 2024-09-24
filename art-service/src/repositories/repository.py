f102ffb0f35edd0fc056db3d3e0460823969610b
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-24 15:24:49 +0500

    Update SQLAlchemyRepository.find_all()
    - Added 'joined_attributes' parameter for joinedload()
    - Enhanced filter_by to accept a list, allowing searches for items matching 'smth in lst'

--- art-service/src/repositories/repository.py
+++ art-service/src/repositories/repository.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 # SQLAlchemy
 from sqlalchemy import insert, select, update, delete, and_
+from sqlalchemy.orm import joinedload
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 # Local
@@ -66,13 +67,18 @@ class SQLAlchemyRepository(AbstractRepository):
 
     async def find_all(self,
                        filter_by: dict,
-                       offset: int | None = None,
-                       limit: int | None = None
+                       offset: int = None,
+                       limit: int = None,
+                       joined_attributes: list[str] = None,
                        ) -> "list[BaseEntity]":
         logger.debug(f"Selecting from {self.model.__name__} with filter: {filter_by}")
         async with db_manager.async_session_maker() as session:
             stmt: "Select" = select(self.model)
-            print(f"offset: {offset}, limit: {limit}")
+            if joined_attributes:
+                for attr in joined_attributes:
+                    stmt: "Select" = stmt.options(joinedload(getattr(self.model, attr)))
+            logger.debug(f"stmt: {stmt}")
+            logger.debug(f"offset: {offset}, limit: {limit}")
             if offset is not None and limit is not None:
                 stmt: "Select" = stmt.offset(offset).limit(limit)
 
@@ -80,8 +86,11 @@ class SQLAlchemyRepository(AbstractRepository):
                 conditions: list[BinaryExpression] = []
                 for key, value in filter_by.items():
                     if hasattr(self.model, key):
-                        # noinspection PyTypeChecker
-                        expression: "BinaryExpression" = getattr(self.model, key) == value
+                        if isinstance(value, list):
+                            expression: "BinaryExpression" = getattr(self.model, key).in_(value)
+                        else:
+                            # noinspection PyTypeChecker
+                            expression: "BinaryExpression" = getattr(self.model, key) == value
                         conditions.append(expression)
                 if conditions:
                     stmt = stmt.where(and_(*conditions))
@@ -90,7 +99,7 @@ class SQLAlchemyRepository(AbstractRepository):
             except SQLAlchemyError as err:
                 logger.error(f"SQLAlchemyError: {err}")
                 raise err
-            rows = result.all()
+            rows = result.unique().all()
             entities: "list[BaseEntity]" = [row[0].to_entity() for row in rows]
             logger.info(f"Found {len(entities)} entities")
             return entities
