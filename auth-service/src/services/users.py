a62c47d9b6e02aae34617d29cdc4ed5ed3e81330
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-28 01:05:05 +0500

    update: added get_all_users() method of UsersService. It retrieves users by list of ID

--- auth-service/src/services/users.py
+++ auth-service/src/services/users.py
@@ -4,11 +4,12 @@ from schemas.users import (
     UserCreateSchema,
     UserLoginSchema,
     UserEntity,
+    UserReadSchema,
 )
 from schemas.tokens import TokenInfoSchema
 from utils.password import hash_password, check_password
 from utils.jwt_utils import create_access_jwt, create_refresh_jwt
-
+from sqlalchemy.exc import SQLAlchemyError
 from exceptions.http import (
     UsernameAlreadyExistHTTPException,
     EmailAlreadyExistsHTTPException,
@@ -46,60 +47,41 @@ class UserService:
         self.user_repo = user_repo
 
     async def add_user(self, user_create_data: UserCreateSchema) -> int:
-        """
-        Adds a new user to the repository.
-
-        Checks if a user with the given username or email already exists. If so, raises an appropriate exception.
-        Validates the password length and raises an exception if the password is too weak.
-
-        Hashes the provided password before storing the new user in the repository.
-
-        @param user_create_data: An instance of `UserCreateSchema` containing the user's username, email, and password.
-        @return: The ID of the newly created user.
-        @raises UsernameAlreadyExistHTTPException: If a user with the specified username already exists.
-        @raises EmailAlreadyExistsHTTPException: If a user with the specified email already exists.
-        @raises WeakPasswordHTTPException: If the provided password is shorter than 6 characters.
-        """
         user_by_username: list[UserEntity] = await self.user_repo.find_all(
             filter_by={"username": user_create_data.username})
+
         if user_by_username:
+            logger.warning(f"Username '{user_create_data.username}' already exists.")
             raise UsernameAlreadyExistHTTPException
 
         user_by_email = await self.user_repo.find_all(filter_by={"email": user_create_data.email})
         if user_by_email:
+            logger.warning(f"Email '{user_create_data.email}' already exists.")
             raise EmailAlreadyExistsHTTPException
 
         if len(user_create_data.password) < 6:
+            logger.warning("Provided password is too weak.")
             raise WeakPasswordHTTPException
 
         hashed_password = hash_password(password=user_create_data.password)
-
         new_user_data: dict = {
             "username": user_create_data.username,
             "email": user_create_data.email,
             "hashed_password": hashed_password,
         }
         new_user_id: int = await self.user_repo.add_one(data=new_user_data)
-
+        logger.info(f"User '{user_create_data.username}' added with ID: {new_user_id}")
         return new_user_id
 
     async def validate_user(self, user: UserLoginSchema) -> TokenInfoSchema:
-        """
-        Validates a user's credentials and generates an authentication token.
-
-        Checks if a user with the provided username exists and is active. Validates the user's password.
-        If any validation fails, raises an appropriate exception. If validation succeeds, generates and returns a token for the user.
-
-        @param user: An instance of `UserLoginSchema` containing the username and password for authentication.
-        @return: An instance of `TokenInfoSchema` containing the authentication token and related information.
-        @raises UnauthorizedHTTPException: If the username does not exist, the user is not active, or the password is incorrect.
-        """
         user_by_username: list[UserEntity] = await self.user_repo.find_all({"username": user.username})
 
         if not user_by_username:
+            logger.warning(f"Unauthorized access attempt for username: '{user.username}'")
             raise UnauthorizedHTTPException
 
         if not user_by_username[0].is_active:
+            logger.warning(f"User '{user.username}' is not active.")
             raise UserNotActiveHTTPException
 
         user_entity: UserEntity = user_by_username[0]
@@ -107,10 +89,11 @@ class UserService:
         check_password_result: bool = check_password(user.password, correct_password)
 
         if not check_password_result:
+            logger.warning(f"Incorrect password attempt for user: '{user.username}'")
             raise UnauthorizedHTTPException
 
         token_data: TokenInfoSchema = create_token_for_user(user=user_entity)
-
+        logger.info(f"Token created for user '{user.username}'")
         return token_data
 
     async def create_token_for_user_by_id(self, user_id: int, include_refresh: bool = False) -> TokenInfoSchema:
@@ -118,4 +101,19 @@ class UserService:
         result_token = create_token_for_user(
             user=user_by_id[0],
             include_refresh=include_refresh)
+        logger.info(f"Token created for user ID: {user_id}")
         return result_token
+
+    async def get_all_users(self, users_id: list[int]):
+        result_users: list[UserEntity] = await self.user_repo.find_all({"id": users_id})
+        users_info: list[dict] = []
+        for user in result_users:
+            user_info = {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "profile_image": user.profile_image,
+            }
+            users_info.append(user_info)
+        logger.info(f"Retrieved information for {len(users_info)} users.")
+        return users_info
