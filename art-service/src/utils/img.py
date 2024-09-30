461a45a2e8bc3af2a24f767bf065a8cb86efe48f
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-09-30 13:23:38 +0500

    Fix: convert_to_jpg() not can convert RGBA images to JPG.

--- art-service/src/utils/img.py
+++ art-service/src/utils/img.py
@@ -9,10 +9,14 @@ async def convert_to_jpg(upload_file: BinaryIO) -> BinaryIO:
     logger.info("Started converting the image")
     try:
         image = Image.open(upload_file)
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
+
         output = io.BytesIO()
         image.save(output, format="JPEG")
         output.seek(0)
         return output
+
     except OSError as err:
-        logger.error("OSError occurred during image processing")
+        logger.error("OSError occurred during image processing: %s", err)
         raise err
