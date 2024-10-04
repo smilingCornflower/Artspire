6e12b2544bda263e3caff877d894cf9f31c33903
nauzybekturganbayev <smiling.cornflower@gmail.com>
2024-10-05 03:49:44 +0500

    Refactor: tests/conftest.py and tests/test_arts.py

--- art-service/tests/conftest.py
+++ art-service/tests/conftest.py
@@ -2,13 +2,11 @@ from typing import TYPE_CHECKING
 import pytest
 import asyncio
 from httpx import AsyncClient
-
+from dotenv import load_dotenv
 from models import Base
-
+import os
 from src.main import app
-
 from src.database.db import db_manager
-
 from src.config import settings
 from src.config import logger
 
@@ -20,6 +18,12 @@ if TYPE_CHECKING:
 
 AUTH_SERVER: str = f"http://{settings.server.host}:{settings.server.auth_port}"
 ARTS_SERVER: str = f"http://{settings.server.host}:{settings.server.arts_port}"
+
+load_dotenv()
+test_user_1_name: str = "test_user_1"
+test_user_2_name: str = "test_user_2"
+test_user_1_password: str = os.getenv("TEST_USER_1_PASS")
+test_user_2_password: str = os.getenv("TEST_USER_2_PASS")
 
 
 # SETUP
@@ -50,14 +54,22 @@ async def async_client() -> "AsyncGenerator[AsyncClient, None]":
         yield ac
 
 
-@pytest.fixture(scope="session", autouse=True)
-async def token() -> str:
-    data = {"username": "test_user", "password": "test_user"}
+async def get_token(data: dict) -> str:
     async with AsyncClient(base_url=AUTH_SERVER) as ac:
         response: "Response" = await ac.post("/users/login", data=data)
 
     assert response.status_code == 200
-
     access_token: str = response.json()["access_token"]
-    logger.debug(f"access_token: {access_token[:50]}")
     return access_token
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def token_1() -> str:
+    user_data_1 = {"username": test_user_1_name, "password": test_user_1_password}
+    return await get_token(user_data_1)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def token_2() -> str:
+    user_data_2 = {"username": test_user_2_name, "password": test_user_2_password}
+    return await get_token(user_data_2)
--- art-service/tests/test_arts.py
+++ art-service/tests/test_arts.py
@@ -1,40 +1,59 @@
 import pytest
 import asyncio
+
+import requests
+from httpx import request
 from typing import TYPE_CHECKING
 from src.config import logger
 from os import path
+from src.services.arts import ArtsService
 
 if TYPE_CHECKING:
-    from httpx import Response
+    from httpx import Response, AsyncClient
     from typing import Any
 
 cur_dir = path.dirname(__file__)
+arts_url: str = "/arts"
 
 
-async def test_post_arts(async_client, token):
+class TestArtsPost:
     query_params: list[dict] = [
         {"art_title": "title_1", "art_tags": "tag1,tag2,tag3"},
         {"art_title": "title_2", "art_tags": "tag3,tag4,tag5"},
         {"art_title": "title_3", "art_tags": "tag5,tag6,tag7"},
     ]
-    headers = {"Authorization": f"Bearer {token}"}
     images: list[dict] = [
         {"art_file": ("image_1.jpg", open(f"{cur_dir}/images/image_1.jpg", "rb"), "image/jpeg")},
         {"art_file": ("image_2.png", open(f"{cur_dir}/images/image_2.png", "rb"), "image/jpeg")},
         {"art_file": ("image_3.webp", open(f"{cur_dir}/images/image_3.webp", "rb"), "image/jpeg")},
     ]
-    for i in range(3):
+
+    async def test_post_arts_without_token(self, async_client: "AsyncClient") -> None:
         response: "Response" = await async_client.post(
-            "/arts", params=query_params[i], headers=headers, files=images[i]
+            arts_url, params=self.query_params[0], files=self.images[0]
         )
-        assert response.status_code == 201
+        assert response.status_code == 401
+
+    async def test_post_arts_with_token(
+            self, async_client: "AsyncClient", token_1: str, token_2: str
+    ) -> None:
+        headers: list[dict] = [{"Authorization": f"Bearer {token_1}"},
+                               {"Authorization": f"Bearer {token_1}"},
+                               {"Authorization": f"Bearer {token_2}"}]
+
+        for i in range(3):
+            response: "Response" = await async_client.post(
+                arts_url, params=self.query_params[i], headers=headers[i], files=self.images[i]
+            )
+            assert response.status_code == 201
 
 
 class TestArtsGet:
-    async def test_without_params(self, async_client):
-        # IMPORTANT: this test depends on test_post_arts()
-        response: "Response" = await async_client.get("/arts")
+    async def test_without_params(self, async_client: "AsyncClient") -> None:
+        # IMPORTANT: this test depends on TestArtsPost()
+        response: "Response" = await async_client.get(arts_url)
         result: "Any" = response.json()
+        logger.debug(f"result GET: {result}")
 
         assert response.status_code == 200
         assert len(result) == 3
@@ -47,7 +66,14 @@ class TestArtsGet:
             (10, 10, None, 404),
         ]
     )
-    async def test_with_offset_limit(self, async_client, offset, limit, length, status_code):
+    async def test_with_offset_limit(
+            self,
+            async_client: int,
+            offset: int,
+            limit: int,
+            length: int,
+            status_code: int
+    ) -> None:
         logger.debug(f"offset: {offset}, limit: {limit}, length: {length}, status: {status_code}")
         query_params: dict[str, int] = {"offset": offset, "limit": limit}
         response: "Response" = await async_client.get("/arts", params=query_params)
@@ -56,3 +82,53 @@ class TestArtsGet:
         assert response.status_code == status_code
         if length:
             assert len(result) == length
+
+    async def test_with_tags(self, async_client: "AsyncClient") -> None:
+        query_params: dict = {"art_id": 1, "include_tags": True, "status_code": 200}
+        response: "Response" = await async_client.get("/arts", params=query_params)
+        result: list[dict] = response.json()
+        art: dict = result[0]
+
+        assert response.status_code == 200
+        assert len(art["tags"]) == 3
+
+
+class TestArtsDelete:
+    async def test_without_token(
+            self,
+            async_client: "AsyncClient"
+    ) -> None:
+        response: "Response" = await async_client.request(
+            method="DELETE", url=arts_url, json={"art_id": 1}
+        )
+        assert response.status_code == 401
+
+    @pytest.mark.parametrize(
+        "art_id, status_code",
+        [(1, 200), (2, 200), (3, 403), (4, 404)]
+    )
+    # art_id=3 was published by test_user_2, so 403(Forbidden) for DELETE(/arts) from test_user_1
+    # art_id=4 does not exist
+    async def test_with_token(
+            self,
+            async_client: "AsyncClient",
+            art_id: int,
+            status_code: int,
+            token_1: str
+    ) -> None:
+        headers = {"Authorization": f"Bearer {token_1}"}
+        body: dict = {"art_id": art_id}
+        response: "Response" = await async_client.request(
+            method="DELETE", url=arts_url, json=body, headers=headers
+        )
+
+        assert response.status_code == status_code
+
+    async def test_third_art(self, async_client, token_2) -> None:
+        headers = {"Authorization": f"Bearer {token_2}"}
+        body: dict = {"art_id": 3}
+        response: "Response" = await async_client.request(
+            method="DELETE", url=arts_url, json=body, headers=headers
+        )
+
+        assert response.status_code == 200
